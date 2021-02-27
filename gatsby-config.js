05c7e3145b1952039b626f99d9e4d547cce05a1e
Todd Birchard <toddbirchard@gmail.com>
2021-02-27 18:39:24 -0500

    Netlify config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,17 +58,17 @@ module.exports = {
         legacy: false,
         icons: [
           {
-            src: `images/icons/icon-192x192.png`,
+            src: `/images/icons/icon-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `images/icons/icon-512x512.png`,
+            src: `/images/icons/icon-512x512.png`,
             sizes: `512x512`,
             type: `image/png`,
           },
           {
-            src: `images/icons/icon-96x96.png`,
+            src: `/images/icons/icon-96x96.png`,
             sizes: `96x96`,
             type: `image/png`,
           },
