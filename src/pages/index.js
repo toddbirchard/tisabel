da3d9ae0d918a327b993bb3bbc4215f9435f5d11
Todd Birchard <toddbirchard@gmail.com>
2021-02-28 17:02:37 -0500

    Test Netlify build without Details widget.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,26 +3,26 @@ import { graphql } from 'gatsby'
 import Img from "gatsby-image"
 import PropTypes from 'prop-types'
 import { Layout } from "../components/common"
-import { Itinerary, Rsvp, Details, Donate } from "../components"
+import { Itinerary, Rsvp, Donate } from "../components"
 import "../styles/main.less"
 
 const Homepage = ({ data }) => {
   const heroImage = data.strapiHomepage.cover_image.childImageSharp.fluid
-  const logoImage = data.strapiHomepage.logo.childImageSharp.fluid
+  // const logoImage = data.strapiHomepage.logo.childImageSharp.fluid
 
   return (
     <>
       <Layout>
         <section className="hero">
-          <div className="hero-title">
+          {/*<div className="hero-title">
             <Img className="logo" fluid={logoImage} />
             <h1>{data.strapiHomepage.title}</h1>
-          </div>
+          </div>*/}
           <Img className="hero-image" fluid={heroImage} />
         </section>
         <main>
           <div className="information">
-            <Details />
+            {/*<Details />*/}
             <Itinerary />
             <Rsvp />
             <Donate />
