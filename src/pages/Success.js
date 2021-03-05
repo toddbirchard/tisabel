749772f7b3abcd5823152606d9667a1c2308dc92
Todd Birchard <toddbirchard@gmail.com>
2021-03-04 22:07:02 -0500

    Success page.

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -4,7 +4,7 @@ import { Layout } from "../components/common"
 const SuccessPage = () => (
   <Layout>
     <h1>Success!</h1>
-    <p>{`We received your gift.. you're the best!.`}</p>
+    <p>{`We received your gift. You're the best!.`}</p>
   </Layout>
 )
 
