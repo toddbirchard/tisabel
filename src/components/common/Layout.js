99f098253ba498c318f44f8ef7b52daf6a578301
Todd Birchard <toddbirchard@gmail.com>
2021-03-07 01:30:27 -0500

    Broken module names.

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import MetaData from "./metadata"
+import MetaData from "./"
 
 
 const Layout = ({ data, template, children }) => {
