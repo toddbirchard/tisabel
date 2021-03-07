1764b4edd56fd6acf41cce07910d0b3689f41953
Todd Birchard <toddbirchard@gmail.com>
2021-03-07 12:29:56 -0500

    Add mapbox worker.

--- src/components/venue/Map.js
+++ src/components/venue/Map.js
@@ -1,4 +1,7 @@
 import ReactMapboxGl from "react-mapbox-gl"
+import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker"
+
+ReactMapboxGl.workerClass = MapboxWorker.default
 
 const Map = ReactMapboxGl({
   accessToken: process.env.GATSBY_MAPBOX_TOKEN,
